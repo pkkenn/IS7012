f12cd74e8d2d91b05b7b546b0d287d72aeb368b4
pkkenn <kennyken128@outlook.com>
2024-09-15 20:57:55 -0400

    recruit

--- Week3/Week 3 Exercise 1/obj/Release/net8.0/Week 3 Exercise 1.AssemblyInfo.cs
+++ Week3/Week 3 Exercise 1/obj/Release/net8.0/Week 3 Exercise 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week 3 Exercise 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9a902d5799ffb324b5080518f99bc5daf05270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30712a3f1db88e87da42bfca36d944dff7f1a9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week 3 Exercise 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week 3 Exercise 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
