fee31365d544c72479b4cbb7b4eb154a2874f012
pkkenn <122116882+pkkenn@users.noreply.github.com>
2025-09-07 21:14:15 -0400

    pass

--- Week 2/Week 2 Exercises/Exercise 2/DiceRoller/obj/Debug/net9.0/DiceRoller.AssemblyInfo.cs
+++ Week 2/Week 2 Exercises/Exercise 2/DiceRoller/obj/Debug/net9.0/DiceRoller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52f44729b4925fa1894d14437d86590f8330360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b4936fab42d72a3889c9fd3ad03e19e9a7057c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
