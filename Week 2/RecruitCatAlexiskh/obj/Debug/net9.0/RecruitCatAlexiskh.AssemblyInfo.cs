b5c0128dea9779052dbacc8955aea8f169d8b933
pkkenn <122116882+pkkenn@users.noreply.github.com>
2025-09-14 16:24:06 -0400

    w3e1

--- Week 2/RecruitCatAlexiskh/obj/Debug/net9.0/RecruitCatAlexiskh.AssemblyInfo.cs
+++ Week 2/RecruitCatAlexiskh/obj/Debug/net9.0/RecruitCatAlexiskh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitCatAlexiskh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f93151eb640adaa5b923a1a086704b2b3d2bfd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed00615e4497e85dac871cec88497f66cf3abf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitCatAlexiskh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitCatAlexiskh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
