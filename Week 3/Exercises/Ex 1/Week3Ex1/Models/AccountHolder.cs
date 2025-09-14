b5ff2f239b3cefb7598a4a5055c8259b9bc6f9ea
pkkenn <122116882+pkkenn@users.noreply.github.com>
2025-09-14 19:54:17 -0400

    accountholder

---
+++ Week 3/Exercises/Ex 1/Week3Ex1/Models/AccountHolder.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Week3Ex1.Models;
+
+public class AccountHolder
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<BankAccount>? BankAccounts { get; set; }
+}
