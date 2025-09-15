413294d11e8769d0243789d4ddc250093ce0d931
alexiskh <alexiskh@mail.uc.edu>
2025-09-15 17:17:13 -0400

    .

--- Week 3/RecruitCatAlexiskh/obj/Release/net9.0/RecruitCatAlexiskh.AssemblyInfo.cs
+++ Week 3/RecruitCatAlexiskh/obj/Release/net9.0/RecruitCatAlexiskh.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitCatAlexiskh")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256eee7bbe0d73b40ce07ede9687be4cec25cf11")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitCatAlexiskh")]
