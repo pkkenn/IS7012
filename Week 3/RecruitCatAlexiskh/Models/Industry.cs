216fb9b828fb5417b27cf5b816998820f662deaf
pkkenn <122116882+pkkenn@users.noreply.github.com>
2025-09-15 10:06:44 -0400

    Update Industry.cs

--- Week 3/RecruitCatAlexiskh/Models/Industry.cs
+++ Week 3/RecruitCatAlexiskh/Models/Industry.cs
@@ -5,6 +5,7 @@ public class Industry
 {
     public int ID { get; set; }
     public string Name { get; set; }
+    public string Sector { get; set; }
     public List<Candidate> Candidates { get; set; }
     public List<Company> Companies { get; set; }
 }
