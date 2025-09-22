b99b94f168decd45b3e243af728ad81a3b8487b3
Pkkenn <kennyken128@gmail.com>
2025-09-21 21:08:30 -0400

    .

--- Week 4/Week3Ex1_2/Models/BankAccount.cs
+++ Week 4/Week3Ex1_2/Models/BankAccount.cs
@@ -1,13 +1,31 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
 
-namespace Week3Ex1_2.Models;
-
-public class BankAccount
+namespace Week3Ex1_2.Models
 {
-    public int ID { get; set; }
-    public string Number { get; set; }
-    public decimal CurrentBalance { get; set; }
-    public string Name { get; set; }
-    public DateTime DateOpened { get; set; }
-    public AccountHolder AccountHolder { get; set; }
+    public class BankAccount
+    {
+        public int ID { get; set; }
+        
+        [Display(Name = "Account Number")]
+        public string Number { get; set; }
+
+        [Display(Name = "Current Balance")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")] 
+        public decimal CurrentBalance { get; set; }
+
+        [Display(Name = "Account Nickname")]
+        public string Name { get; set; }
+
+        [Display(Name = "Date Opened")]
+        [DataType(DataType.Date)]
+        public DateTime DateOpened { get; set; }
+
+        [Display(Name = "Account Holder")]
+        public int AccountHolderId { get; set; }
+
+        public AccountHolder AccountHolder { get; set; }
+    }
 }
